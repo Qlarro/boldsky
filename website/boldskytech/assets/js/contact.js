f0ee6515bd6f260ec07343feb4fefa0db601634f
BenMaddox <maddox_benjamin@yahoo.com>
2016-05-14 21:59:37 -0700

    Many many changes
    Getting in sync with repo

--- website/boldskytech/assets/js/contact.js
+++ website/boldskytech/assets/js/contact.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+
     /* ======= jQuery form validator ======= */ 
     /* Ref: http://jqueryvalidation.org/documentation/ */   
     $("#contact-form").validate({
@@ -22,5 +22,25 @@ $(document).ready(function() {
 	
 });
 
+	//TODO Move to a JS file...or better yet probably just want to move to angular controller
+	var getUrlParameter = function getUrlParameter(sParam) {
+		var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
+
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+
+			if (sParameterName[0] === sParam) {
+				return sParameterName[1] === undefined ? true : sParameterName[1];
+			}
+		}
+		};
+		
+	if (getUrlParameter('fwd') == 'cd'){
+		console.log("Found param")
+		$("#contactSuccess").removeClass("hidden");
+	}
 
 
